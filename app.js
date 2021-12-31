6ecfa7c3945c90e180289baf0486a0235fc06152
向山　翔 <hms0175km@gmail.com>
2021-12-31 17:58:55 +0900

    [Add]postman

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
+app.post('/booklog', (req, res) => {
+    const booklog = req.body
+    res.json({
+        "ok": true,
+        "booklog": booklog
+    })
+})
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
